11af34efbff126504db8ad21d6e629ff69e2ca4b
Guilherme Almeida de Almeida <guialmeida@gmail.com>
2014-11-16 13:47:53 -0500

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,12 @@
+class WelcomeController < ApplicationController
+  
+
+  def index
+    @websites =[
+    	["http://ruby.bastardsbook.com/toc/", "The bastards book of Ruby"], 
+    	["https://en.wikibooks.org/wiki/Ruby_Programming", "Wikibooks"],
+    	["http://guides.rubyonrails.org", "Ruby on Rails Guides"] 
+    ]
+@entries = Entry.all
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Diary::Application.config.secret_key_base = 'ee22dbb7ec9a5996ba1f7e92672c5b8160317326adbe7830a8d73e2705593664d040de1c111c99dc39b382cf0629722a5e80e6eec4786f368ee3bb367ca3dc27'
